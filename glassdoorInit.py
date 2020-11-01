a7d9e8f00ae8dd63a05b06db1a18f26fb1b4c09e
zhangyang2017 <zhangya3@oregonstate.edu>
2020-10-31 17:02:41 -0700

    update file

--- glassdoorInit.py
+++ glassdoorInit.py
@@ -10,4 +10,5 @@ import glassdoor_scraper as gs
 
 path = '/Users/yangzhang/document/ds_projs/ppj_JobScraping/chromedriver'
 
-df = gs.get_jobs('data scientist', 30, True, path, 10)
+df = gs.get_jobs('data scientist', 1241, True, path, 10)
+df.to_csv('./data/glassdoor_raw_dataScientist.csv', index=False)
